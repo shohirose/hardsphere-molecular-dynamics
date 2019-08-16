108bd406837babd13f86b150422715a1f749169a
Sho Hirose <sho.hirose@japex.co.jp>
2019-08-16 16:20:45 +0900

    test: modify unit tests for collision time to use particle classes and to add edge cases

--- test/collision.cpp
+++ test/collision.cpp
@@ -3,34 +3,34 @@
 
 TEST_CASE("collisions of hardsphere particles", "[collision_time]") {
   using hardsphere::collision_time;
-  using hardsphere::vector2d;
+  using hardsphere::particle;
 
   SECTION("two particles collide") {
-    vector2d x1(-1.0, 0.0);
-    vector2d v1(1.0, 0.0);
-
-    vector2d x2(1.0, 0.0);
-    vector2d v2(-1.0, 0.0);
-
-    double a1 = 0.01;
-    double a2 = 0.01;
-
-    const auto t = collision_time(x1, v1, a1, x2, v2, a2);
+    particle p1({-1.0, 0.0}, {1.0, 0.0}, 0.01);
+    particle p2({1.0, 0.0}, {-1.0, 0.0}, 0.01);
+    const auto t = collision_time(p1, p2);
     REQUIRE(t.has_value());
     REQUIRE(*t == Approx(0.99));
   }
 
   SECTION("two particles do not collide") {
-    vector2d x1(-1.0, 0.0);
-    vector2d v1(0.0, 1.0);
+    particle p1({-1.0, 0.0}, {0.0, 1.0}, 0.01);
+    particle p2({1.0, 0.0}, {0.0, -1.0}, 0.01);
+    const auto t = collision_time(p1, p2);
+    REQUIRE(!t.has_value());
+  }
 
-    vector2d x2(1.0, 0.0);
-    vector2d v2(0.0, -1.0);
+  SECTION("two particles move in parallel") {
+    particle p1({-1.0, 0.0}, {0.0, 1.0}, 0.01);
+    particle p2({1.0, 0.0}, {0.0, 1.0}, 0.01);
+    const auto t = collision_time(p1, p2);
+    REQUIRE(!t.has_value());
+  }
 
-    double a1 = 0.01;
-    double a2 = 0.01;
-
-    const auto t = collision_time(x1, v1, a1, x2, v2, a2);
+  SECTION("two particles move in opposite direction") {
+    particle p1({-1.0, 0.0}, {-1.0, 0.0}, 0.01);
+    particle p2({1.0, 0.0}, {1.0, 0.0}, 0.01);
+    const auto t = collision_time(p1, p2);
     REQUIRE(!t.has_value());
   }
 }
