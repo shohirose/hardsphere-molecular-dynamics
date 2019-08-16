d15056e11354eca40113acf65a83ebb1a3c6e8e1
Sho Hirose <sho.hirose@japex.co.jp>
2019-08-16 16:18:00 +0900

    feat: create particle class

--- include/hardsphere/collision.hpp
+++ include/hardsphere/collision.hpp
@@ -2,7 +2,7 @@
 #define HARDSPHERE_COLLISION_HPP
 
 #include <optional>
-#include "hardsphere/vector2d.hpp"
+#include "hardsphere/particle.hpp"
 
 namespace hardsphere {
 
@@ -18,6 +18,15 @@ std::optional<double> collision_time(const vector2d& x1, const vector2d& v1,
                                      double a1, const vector2d& x2,
                                      const vector2d& v2, double a2);
 
+/// @brief Computes collision time of two particles
+/// @param[in] p1 Particle 1
+/// @param[in] p2 Particle 2
+/// @return Collision time
+inline std::optional<double> collision_time(const particle& p1,
+                                            const particle& p2) {
+  return collision_time(p1.position, p1.velocity, p1.radius, p2.position,
+                        p2.velocity, p2.radius);
+}
 
 }  // namespace hardsphere
 
---
+++ include/hardsphere/particle.hpp
@@ -0,0 +1,34 @@
+#ifndef HARDSPHERE_PARTICLE_HPP
+#define HARDSPHERE_PARTICLE_HPP
+
+#include "hardsphere/vector2d.hpp"
+
+namespace hardsphere {
+
+struct particle {
+  vector2d position;
+  vector2d velocity;
+  double radius;
+
+  particle() = default;
+  particle(const vector2d& t_position, const vector2d& t_velocity,
+           double t_radius)
+      : position{t_position}, velocity{t_velocity}, radius{t_radius} {}
+  particle(const particle&) = default;
+  particle(particle&&) = default;
+
+  particle& operator=(const particle&) = default;
+  particle& operator=(particle&&) = default;
+};
+
+inline std::istream& operator>>(std::istream& is, particle& p) {
+  return is >> p.position >> p.velocity >> p.radius;
+}
+
+inline std::ostream& operator<<(std::ostream& os, const particle& p) {
+  return os << p.position << ' ' << p.velocity << ' ' << p.radius;
+}
+
+}  // namespace hardsphere
+
+#endif  // HARDSPHERE_PARTICLE_HPP
