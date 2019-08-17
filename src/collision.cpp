24c85c6307107ee22406c7c9d55c65823b2550f3
Sho Hirose <sho.hirose@gmail.com>
2019-08-17 11:42:30 +0900

    feat: create collide function to update particle position and velocity by assuming elastic collision.

--- src/collision.cpp
+++ src/collision.cpp
@@ -2,9 +2,9 @@
 
 namespace hardsphere {
 
-std::optional<double> collision_time(const vector2d& x1, const vector2d& v1,
-                                     double a1, const vector2d& x2,
-                                     const vector2d& v2, double a2) {
+std::optional<double> collision_time(                   //
+    const vector2d& x1, const vector2d& v1, double a1,  //
+    const vector2d& x2, const vector2d& v2, double a2) {
   const auto dx = x1 - x2;
   const auto dv = v1 - v2;
   const auto b = dx.dot(dv);
@@ -25,6 +25,19 @@ std::optional<double> collision_time(const vector2d& x1, const vector2d& v1,
     else
       return t;
   }
+}
+
+void collide(vector2d& x1, vector2d& v1, double m1,  //
+             vector2d& x2, vector2d& v2, double m2,  //
+             double tc) {
+  const auto dx = x1 - x2;
+  const auto dv = v1 - v2;
+  const auto b = dx.dot(dv);
+  const auto rsqr = dx.squared_norm();
+  x1 = new_position(x1, v1, tc);
+  x2 = new_position(x2, v2, tc);
+  v1 -= 2 * b / rsqr * (m2 / (m1 + m2)) * dx;
+  v2 += 2 * b / rsqr * (m1 / (m1 + m2)) * dx;
 }
 
 }  // namespace hardsphere
